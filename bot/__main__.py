12b1eeec7a7e4f032185f5878ce8b18089bb09bf
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2022-06-15 03:49:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -109,9 +109,9 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton(
-        "Repo", "https://www.github.com/yashoswalyo/clone-bot-public"
+        "Leech/Mirror Group ❤", "https://t.me/ashish_leech_group"
     )
-    buttons.buildbutton("Report Group", "https://t.me/yash_codes_support")
+    buttons.buildbutton("Owner 🖤", "https://t.me/ashishsharmalegend")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f"""
@@ -121,7 +121,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
         sendMarkup(
-            "Not Authorized user, deploy your own clone bot",
+            "Not Authorized user, Join below group to use me @ashish_leech_group ",
             context.bot,
             update.message,
             reply_markup,
